8239cda76f9e59912e88a51caab9a8163fb5c320
souhail-nehos <souhail.tourjmen@nehos-groupe.com>
2024-05-11 21:35:26 +0100

    - create bottom tab - add new navigation for bottom tab issue #13

--- src/navigation/config/index.ts
+++ src/navigation/config/index.ts
@@ -27,12 +27,37 @@ export const ARRAYROUTES = [
     component: ForgetPasswordScreen,
     options:{ headerShown: false }
   },
+ 
+  
+  
+];
+export const BottomRoutes = [
   {
     route: ROUTES.HomeScreen,
-    label: ROUTES.HomeScreen,
+    label: i18n.t(TEXT.ROUTES.home),
     component: HomeScreen,
-    options:{ headerShown: false }
+    options:{ headerShown: false ,icon:'home'}
     
+  },
+  {
+    route: ROUTES.MESSAGESCREENBOTTOM,
+    label: i18n.t(TEXT.ROUTES.message),
+    component: HomeScreen,
+    options:{ headerShown: false ,icon:'comments'}
+    
+  },
+  {
+    route: ROUTES.MYCOURSESCREENBOTTOM,
+    label: i18n.t(TEXT.ROUTES.myCourse),
+    component: HomeScreen,
+    options:{ headerShown: false ,icon:'book'}
+    
+  },
+  {
+    route: ROUTES.PROFILESCREENBOTTOM,
+    label: i18n.t(TEXT.ROUTES.profile),
+    component: HomeScreen,
+    options:{ headerShown: false ,icon:'user'}    
   },
   
   
